edf826ba19847c14e1b8334f0f16d734e6bd8144
VanAnhPH13707 <79831956+VanAnhPH13707@users.noreply.github.com>
2022-02-22 16:13:21 +0700

    update

--- my-app/src/Pages/home.js
+++ my-app/src/Pages/home.js
@@ -1,6 +1,7 @@
 import Footer from "../components/footer";
 import Header from "../components/header";
 import NewList from "../components/newsList";
+import ProductList from "../components/productList";
 
 const HomePage = {
     async render() {
@@ -8,11 +9,19 @@ const HomePage = {
         ${Header.render()}
         <div class="max-w-5xl mx-auto">
             <div class="news">
+                ${await ProductList.render()}
+            </div>
+        </div>
+        <div class="max-w-5xl mx-auto">
+            <div class="news">
                 ${await NewList.render()}
             </div>
         </div>
         ${Footer.render()}
     `;
+    },
+    afterRender() {
+        Header.afterRender();
     },
 };
 export default HomePage;
